d8e42a6db0df05fa878ea2710088661d2f09d188
haedal1005 <tmdgh9671@daum.net>
2021-01-18 22:23:37 +0900

    컴포넌트 파일 경로 수정

--- vue_haedal/src/App.vue
+++ vue_haedal/src/App.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script>
-import Header from "@/components/layout/Header.vue"
-import Footer from "@/components/layout/Footer.vue"
+import Header from "@/components/Header/Header.vue"
+import Footer from "@/components/Footer/Footer.vue"
 
 export default {
   name: 'App',
--- vue_haedal/src/components/Content/View/SignIn.vue
+++ vue_haedal/src/components/Content/View/SignIn.vue
@@ -26,5 +26,5 @@ export default {}
 </script>
 
 <style>
-@import '../../assets/style/signin.css';
+@import '../../../assets/style/signin.css';
 </style>
