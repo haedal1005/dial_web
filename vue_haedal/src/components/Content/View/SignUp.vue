3c5eae09d6388c643dc2109e0f48026cd0f551f0
yuhyun00 <yuhyun.dev@gmail.com>
2021-01-28 15:19:53 +0900

    회원가입 수정

--- vue_haedal/src/components/Content/View/SignUp.vue
+++ vue_haedal/src/components/Content/View/SignUp.vue
@@ -1,5 +1,22 @@
-<template lang="html">
-  <h1>회원가입</h1>
+<template>
+<div class="SignUpContent">
+  <h1>회원 가입</h1>
+  <form id="SignUpForm">
+    <div class="inputArea" id="nameArea">
+      <input type="text" id="name" class="inputTag" placeholder="이름">
+    </div>
+    <div class="inputArea" id="idArea">
+      <input type="text" id="id" class="inputTag" placeholder="아이디">
+    </div>
+    <div class="inputArea" id="pwArea">
+      <input type="password" id="pw" class="inputTag" placeholder="비밀번호">
+    </div>
+    <div class="inputArea" id="pwArea">
+      <input type="password" id="pw" class="inputTag" placeholder="비밀번호 재확인">
+    </div>
+    <input type="submit" id="SignUpBtn" value="가입하기">
+  </form>
+</div>
 </template>
 
 <script>
@@ -7,5 +24,6 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style>
+@import '../../../assets/style/signUp.css';
 </style>
